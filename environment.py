0fe5cf20bd0d1d43e56db8b3cdcdaab4cb527da8
kywch <choe.kyoung@gmail.com>
2024-08-02 14:51:57 -0700

    updated env, cfg

--- environment.py
+++ environment.py
@@ -2,6 +2,7 @@
 
 import functools
 import numpy as np
+import gymnasium
 
 import pufferlib
 import pufferlib.emulation
@@ -18,14 +19,26 @@ def make_env(name):
     """Create an environment by name"""
     env = gym.make(name)
 
-    # override observation space
-    env.observation_space = gym.spaces.Box(
-        low=env.observation_space.low[0],
-        high=env.observation_space.high[0],
-        shape=env.observation_space.shape,
-        dtype=np.float64,
-    )
+    # TODO: find ways to override DEFAULT_RWD_KEYS_AND_WEIGHTS
+    # reward weights: env.unwrapped.rwd_keys_wt
 
+    env = MyoWrapper(env)
     env = pufferlib.postprocess.ClipAction(env)
     env = pufferlib.postprocess.EpisodeStats(env)
     return pufferlib.emulation.GymnasiumPufferEnv(env=env)
+
+
+class MyoWrapper(gymnasium.Wrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        # override observation space
+        self._observation_space = gym.spaces.Box(
+            low=env.observation_space.low[0],
+            high=env.observation_space.high[0],
+            shape=env.observation_space.shape,
+            dtype=np.float64,  # change from float32, which causes type error
+        )
+
+    def step(self, action):
+        obs, reward, done, truncated, info = self.env.step(action)
+        return obs, reward, done, truncated, info
