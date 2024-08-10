4ef14b36f8f42ecf3a08a349e15d9c516138a1a4
kywch <choe.kyoung@gmail.com>
2024-08-10 08:56:21 -0700

    add run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+python train.py -e myoElbowPose1D6MRandom-v0 --vec multiprocessing --track
+python train.py -e myoFingerPoseRandom-v0 --vec multiprocessing --track
+python train.py -e myoFingerReachRandom-v0 --vec multiprocessing --track
+python train.py -e myoHandKeyTurnRandom-v0 --vec multiprocessing --track
+python train.py -e myoHandObjHoldRandom-v0 --vec multiprocessing --track
+python train.py -e myoHandPenTwirlRandom-v0 --vec multiprocessing --track
+python train.py -e myoHandPoseRandom-v0 --vec multiprocessing --track
+python train.py -e myoHandReachRandom-v0 --vec multiprocessing --track
